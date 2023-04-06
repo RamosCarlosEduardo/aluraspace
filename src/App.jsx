28b9b059f4caa9d8ff267a17a48413a330b842c5
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2023-04-06 17:00:06 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './styles/global.scss'
 import Home from "./pages/home"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   
   return (
     <div className="App">
+      <Header />
       <Home />
+      <Footer />
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,20 @@
 import Banner from "../../components/Banner";
-import Header from "../../components/Header";
 import Menu from "../../components/Menu";
+import Galery from "../../components/Galery";
 import styles from './Home.module.scss'
 
 function Home() {
   return (
     <>
-      <Header />
       <main className={styles.principal}>
         <section>
           <Menu />
         </section>
-		<Banner />
+		    <Banner />
       </main>
+      <div>
+        <Galery />
+      </div>
     </>
   );
 }
